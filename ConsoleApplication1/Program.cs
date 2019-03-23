a199497acb927a66b4b18c7798a5094762ac9695
marcelocibils <marcelocibilsuaa@gmail.com>
2019-03-22 21:03:57 -0300

    otra vez

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Mundo Otra Vez");
         }
     }
 }
